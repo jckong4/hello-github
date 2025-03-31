83c33b83751cb980dcd8cbae6bd8cad9be59d57e
Jean Christian Nanan <Jnanan40@gmail.com>
2025-03-31 15:30:46 -0400

    ajout de contenu fichier main

---
+++ main.cs
@@ -0,0 +1,10 @@
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        string nom = "Jean Christian"; // Remplacez par votre nom
+        Console.WriteLine($"Hello, GitHub! Je m'appelle Jean Christian ");
+    }
+}
